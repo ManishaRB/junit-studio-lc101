5660dea3d0763f2333067d5ead3b49a006f1ca09
Manisha B <bholemr@gmail.com>
2022-02-03 11:12:30 -0500

    code updated

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -24,10 +24,13 @@ public class BalancedBrackets {
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
         for (char ch : str.toCharArray()) {
-            if (ch == '[' && brackets >=0) {
+            if (ch == '[') {
                 brackets++;
-            } else if (ch == ']' && brackets >= 0) {
+            } else if (ch == ']') {
                 brackets--;
+            }
+            if (brackets < 0) {
+                return false;
             }
         }
         return brackets == 0;
