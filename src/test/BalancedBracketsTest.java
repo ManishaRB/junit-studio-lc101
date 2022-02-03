6b176ef88885b54651c481e1824718f04e71730f
Manisha B <bholemr@gmail.com>
2022-02-03 10:02:47 -0500

    test cases added

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,65 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
-    @Test
+   @Test
     public void emptyTest() {
-        assertEquals(true, true);
+       assertEquals(true, true);
+   }
+
+   @Test
+   public void emptyString() {
+       assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+   }
+
+    @Test
+    public void onlyBracketsWithCorrectPairReturnsTrue() {
+       assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void emptyBracketsFollowedByString(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void stringFollowedByBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void correctOrderOfBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void twoBracketsInIncorrectOrder() {
+
+       assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void missingCloseBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void bracketsInIncorrectOrderWithString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void onlyOpeningBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyClosingBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void BracketsInIncorrectOrderWithString() {
+       assertFalse(BalancedBrackets.hasBalancedBrackets("]LaunchCode["));
+    }
 
 }
